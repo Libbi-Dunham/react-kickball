ad3ae347dbc7ad04976a97369318f732b0bdb97a
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-16 14:43:16 -0800

    added pathing for team details

--- src/Views/Teams/TeamsList.js
+++ src/Views/Teams/TeamsList.js
@@ -1,7 +1,15 @@
 import { useEffect, useState } from 'react';
 import Team from '../../Components/Team/Teams';
 import { getTeams } from '../../services/TeamRoute';
+import { Link } from 'react-router-dom';
 
 export default function TeamsList() {
-  return <h1>Names of Teams</h1>;
+  return (
+    <ul>
+      <h1>Names of Teams</h1>
+      <p>
+        <Link to={`/teams/:id`}>Team Details</Link>
+      </p>
+    </ul>
+  );
 }
