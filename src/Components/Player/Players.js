6ec8d04218371bcbb0f0eb780d0e23c626de6e5f
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-17 12:42:41 -0800

    fixed so the team details is not on the team page but just on the details page

--- src/Components/Player/Players.js
+++ src/Components/Player/Players.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Player({ Player: { name, position, team_id } }) {
   return (
     <div className="player">
-      {name} {position} {team_id}
+      <Link to={`/players/${team_id}`}>
+        <p>Name: {name}</p>
+      </Link>
+      <p> Position: {position}</p>
+      <p> Team: {team_id} </p>
     </div>
   );
 }
--- src/Components/Team/Teams.js
+++ src/Components/Team/Teams.js
@@ -4,9 +4,8 @@ import React from 'react';
 export default function Teams({ team: { id, name, city, state } }) {
   return (
     <article className="team">
-      <Link to={`/teams/${id}`}>
-        <p>Name: {name}</p>
-      </Link>
+      <Link to={`/teams/${id}`}></Link>
+      <p>Name: {name}</p>
       <p>City: {city}</p>
       <p>State: {state}</p>
     </article>
