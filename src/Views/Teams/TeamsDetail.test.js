e5293b78054bc1092a9cbe5edf2d3fa7afcb8ea8
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-17 16:56:05 -0800

    third test

--- src/Views/Teams/TeamsDetail.test.js
+++ src/Views/Teams/TeamsDetail.test.js
@@ -1,7 +1,18 @@
-// import { render } from '@testing-library/react';
-// import TeamsDetail from './TeamsDetail';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import Team from './TeamsDetail';
 
-// test('renders the team details', () => {
-//   const container = render(<TeamsDetail id={[]} />);
-//   expect(container).toMatchSnapshot();
-// });
+test('renders the team details', () => {
+  const container = render(
+    <MemoryRouter>
+      <Team
+        city="Portland"
+        state="OR"
+        id="1"
+        name="Bridge City Sneakers"
+        players={[{ name: 'Hans Olo' }]}
+      />
+    </MemoryRouter>
+  );
+  expect(container).toMatchSnapshot();
+});
