62c6e33431d5bd0fd3b6e36e945d53749e3b8764
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-17 17:21:58 -0800

    styling

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to Kickball Leagues</h1>
+      <img
+        src="https://i.pinimg.com/736x/75/23/59/752359f299d370ed85c8d46b0c4863ae--exercise-sport.jpg"
+        alt="kickball"
+      ></img>
     </div>
   );
 }
