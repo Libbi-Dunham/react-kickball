d17923bb66eacc05700d16263d36bcf09d6492b2
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-03 15:00:37 -0800

    removed console

--- src/Components/Team/Teams.js
+++ src/Components/Team/Teams.js
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 
 export default function Teams({ team: { id, name, city, state, players } }) {
-  console.log(players);
   return (
     <article className="team">
       <Link to={`/teams/${id}`}></Link>
